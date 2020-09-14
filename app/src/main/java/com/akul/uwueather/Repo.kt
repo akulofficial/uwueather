0d5b416ff586ae1299c9a0674d585291f09fb4ef
Akul Gulrajani <gulrajani.2@osu.edu>
2020-09-13 20:51:21 -0400

    Added and linked model layer of MVVM

--- app/src/main/java/com/akul/uwueather/Repo.kt
+++ app/src/main/java/com/akul/uwueather/Repo.kt
@@ -1,20 +1,32 @@
 package com.akul.uwueather
 
 import android.content.Context
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.beust.klaxon.lookup
 import java.io.IOException
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 class Repo {
 
-    fun loadCurrentWeather(context: Context, fileName: String): String? {
-        val jsonString: String
-        try {
-            jsonString = context.assets.open(fileName).bufferedReader().use{ it.readText() }
-        } catch(ioException: IOException) {
-            ioException.printStackTrace()
-            return null
+    fun loadCurrentWeather(context: Context): WeatherModel {
+        //convert JSON file to deserializable string
+        val currentWeatherJson = context.assets.open("weather.json").bufferedReader().use {
+            it.readText()
         }
-        return jsonString
+
+        //build JSON from generated string
+        val stringBuilder: StringBuilder = StringBuilder(currentWeatherJson)
+        val parser: Parser = Parser.default()
+        val json: JsonObject = parser.parse(stringBuilder) as JsonObject
+
+        //go down one level of JSON to access "data" object
+        val data: JsonArray<String> = json.lookup("data")
+
+        //extract needed values for model object
+        val temp: Int = data.int("temp")[0] as Int
+        val summary: String = data.obj("weather").string("description")[0] as String
+
+        return WeatherModel(summary, temp)
     }
 }
---
+++ app/src/main/java/com/akul/uwueather/Utils.kt
@@ -0,0 +1,8 @@
+package com.akul.uwueather
+
+import android.content.Context
+import java.io.IOException
+
+class Utils {
+
+}
